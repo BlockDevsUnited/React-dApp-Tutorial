339ee93d7569132437a91ea99b5677c2affecf6e
hfccr <hfccr@outlook.com>
2021-08-07 00:46:46 +0530

    Remove truffle migrations

--- migrations/1_initial_migration.js
+++
@@ -1,6 +0,0 @@
-/* global artifacts */
-const Migrations = artifacts.require('Migrations');
-
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
-};
--- migrations/2_deploy_token.js
+++
@@ -1,6 +0,0 @@
-/* global artifacts */
-const BDUToken = artifacts.require('BDUToken');
-
-module.exports = async (deployer) => {
-  await deployer.deploy(BDUToken);
-};
